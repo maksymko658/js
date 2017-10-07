467df957e00db6af56ea0c513546e661c2bb891d
maksymko658 <Jmaksy@ukr.net>
2017-10-08 02:54:37 +0300

    FINISH

--- dom.js
+++ dom.js
@@ -1,182 +1,67 @@
-// EXAMINE THE DOCUMENT OBJECT //
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
-//console.dir(document);
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
 
-/*console.log(document.domain);
-console.log(document.URL);
-console.log(document.title);
-// document.title = 123;
-console.log(document.doctype);
-console.log(document.all);
-console.log(document.body);
-console.log(document.all[10]);
-// document.all[10].textContent = 'Hello';  Change the element with id 10
-console.log(document.forms);
-console.log(document.links);*/
+// Add item
+function addItem(e){
+  e.preventDefault();
 
-// GETELEMENTBYID // 
+  // Get input value
+  var newItem = document.getElementById('item').value;
 
-/*let headerTitle = document.getElementById('header-title');
-console.log(headerTitle);
-headerTitle.textContent = 'Hello';
-headerTitle.innerText = 'Goodbye'; // Pays atention on Style, so hired text with syle="display:none" it will show
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
 
-let header = document.getElementById('main-header');
-header.style.borderBottom = 'solid 3px #000';
+  // Create del button element
+  var deleteBtn = document.createElement('button');
 
-var x = 100;
-var y = 20;
-var c = 100%20;
-console.log(c);*/
+  // Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
 
-// GetElementByClass //
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
 
-// let items = document.GetElementByClassName('list-group-item');
-// console.log(items[1]);
-// items[1].textContent = 'Hello 2';
-// items[1].style.fontWeight = 'bold';
-// items[1].style.backgroundColor = 'yellow';
-// // Gives err
-// // items.style.backgroundColor = 'f4f4f4';
+  // Append button to li
+  li.appendChild(deleteBtn);
 
-// for(let i = 0; i < items.length; i++){
-// 	items[i].style.backgroundColor = 'f4f4f4';
-// }
-//===========================================================COURSE 2================================================================
-//Travesing the DOM //
-var itemList = document.querySelector('#items');
-//parentNode
-// console.log(itemList.parentNode);
-// itemList.parentNode.style.backgroundColor = '#f4f4f4';
-// console.log(itemList.parentNode.parentNode); //container class
-
-/*parentElement*/
-// console.log(itemList.parentElement);
-// itemList.parentElement.style.backgroundColor = '#f4f4f4';
-// console.log(itemList.parentElement.parentElement); //container class
-
-
-//ChildNodes
-//console.log(itemList.childNodes);
-// console.log(itemList.children);
-// console.log(itemList.children[1]);
-// itemList.children[1].style.backgroundColor = 'yellow';
-
-// //FirstChild
-// console.log(itemList.firstChild); //delete spacing
-// //firstElementChild
-// console.log(itemList.firstElementChild);
-// itemList.firstElementChild.textContent = 'Hello';
-
-//lastChild
-// console.log(itemList.lastChild); //delete spacing
-// //lastElementChild
-// console.log(itemList.lastElementChild);
-// itemList.lastElementChild.textContent = 'Hello';
-
-
-// nextSibling// старший брат
-// console.log(itemList.nextSibling);
-// // next ElementSibling
-// console.log(itemList.nextElementSibling);
-
-// //previousSibling
-// console.log(itemList.previousSibling);
-// //prebiousElementSibling
-// console.log(itemList.previousElementSibling);
-// itemList.previousElementSibling.style.backgroundColor = 'green';
-
-// createElement
-
-// create a div
-// let newDiv = document.createElement('div'); 
-// //new Class
-// newDiv.className = 'hello';
-// //new ID
-// newDiv.id = 'hello1';
-// // new title
-// newDiv.setAttribute('title', 'hello Div');
-// //create text node
-// let newDivText = document.createTextNode('hello');
-// //Add text to the div
-//  newDiv.appendChild(newDivText);
-
-// //insure in DOM
-// let container = document.querySelector('header .container');
-// let h1 = document.querySelector('header h1');
-// container.insertBefore(newDiv, h1);
-// newDiv.style.fontSize = '30px';
-
-// console.log(newDiv);
-
-//===========================================================COURSE 3================================================================
-// let button = document.getElementById('button').addEventListener('click', buttonClick);
-// function buttonClick(e){
-// 	// console.log('button clicked');
-// 	// document.getElementById('header-title').textContent = 'Changed';
-// 	// document.querySelector('#main').style.backgroundColor = '#f4f4f4';
-// // console.log(e.target);
-// // console.log(e.target.id);
-// // console.log(e.target.className);
-// // console.log(e.target.classList);
-// // let output = document.getElementById('output');
-// // output.innerText = '<h3>'+e.target.id+'</h3>';
-// // };
-
-// // console.log(e.type);
-// // console.log(e.clientX);//from all page
-// // console.log(e.clientY);
-// // console.log(e.offsetX);// from button
-// // console.log(e.offsetY);
-// // console.log(e.altKey);
-// // console.log(e.shiftKey);
-// // console.log(e.ctrlKey);
-// };
-var button = document.getElementById('button');
-var box = document.getElementById('box');
-
-//button.addEventListener('click', runEvent);
-//button.addEventListener('dblclick', runEvent);
-//button.addEventListener('mousedown', runEvent);
-//button.addEventListener('mouseup', runEvent);
-
-//box.addEventListener('mouseenter', runEvent);
-//box.addEventListener('mouseleave', runEvent);
-
-//box.addEventListener('mouseover', runEvent);
-//box.addEventListener('mouseout', runEvent);
-
-// box.addEventListener('mousemove', runEvent);
-
-var itemInput = document.querySelector('input[type="text"]');
-var form = document.querySelector('form');
-var select = document.querySelector('select');
-
-// itemInput.addEventListener('keydown', runEvent);
-// itemInput.addEventListener('keyup', runEvent);
-// itemInput.addEventListener('keypress', runEvent);
-
-// itemInput.addEventListener('focus', runEvent);
-// itemInput.addEventListener('blur', runEvent);
-
-// itemInput.addEventListener('cut', runEvent);
-// itemInput.addEventListener('paste', runEvent);
-
-// itemInput.addEventListener('input', runEvent);
-
-// select.addEventListener('change', runEvent);
-// select.addEventListener('input', runEvent);
-
-form.addEventListener('submit', runEvent);
-
-
-function runEvent(e){
-	e.preventDefault();
-	console.log('EVENT TYPE: '+e.type);
-// console.log(e.target.value);
-// document.getElementById('output').innerHTML = e.target.value;
-	// output.innerHTML = '<h3>MouseX: '+e.offsetX+' </h3><h3>MouseY: '+e.offsetY+'</h3>';
-	// document.body.style.backgroundColor = "rgb("+e.offsetX+","+e.offsetY+",40)"//CHANGING COLORS in box and body
+  // Append li to list
+  itemList.appendChild(li);
 }
 
-//===========================================================COURSE 4================================================================
+// Remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+
+// Filter Items
+function filterItems(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
